d183e395a7268dcf07e54d1f72585111ac2de1af
baiy <wo@baiy.org>
2015-09-04 16:52:06 +0800

    修改模板处理类 增加 模板缓存和模板继承支持

--- Template.php
+++ Template.php
@@ -3,11 +3,38 @@ namespace Baiy;
 
 /**
  * 模板处理类
+ * $template = new \Baiy\Template($option,Closure $tpl_fun);
+ * assign|get|display|displayJs|fetch
+ * 支持模板继承 extend|block
  */
 class Template {
+	private $option = array(
+		'http_cache_control' => 'private', // 网页缓存控制
+		'cache_path' => '', // 模板缓存路径
+		'tpl_path' => '', // 模板路径
+		'tpl_cache_time' => 0, // 模板缓存时间
+		'suffix' => '.html', // 模板文件后缀
+		'cache_suffix' => '.php', // 模板文件后缀
+		'debug' => false,
+	);
 	private $vars = array();
 
-	public function __construct() {}
+	//模板标识符处理方法(匿名函数)
+	private $tpl_fun;
+
+	public function __construct($option = array(),\Closure $tpl_fun = NULL) {
+		$this->option = array_merge($this->option, $option);
+		if (empty($this->option['tpl_path'])) {
+			throw new \Exception('模板目录没有设置');
+		}
+
+		$this->option['tpl_path'] = rtrim($this->option['tpl_path'], '/\\') . '/';
+		$this->option['cache_path'] = rtrim($this->option['cache_path'], '/\\') . '/';
+
+		define('BAIY_TEMPLATE', True);
+		$this->tpl_fun = $tpl_fun;
+	}
+
 	/**
 	 * 模板变量赋值
 	 * @param  string|array $name  变量名|数组赋值
@@ -36,64 +63,178 @@ class Template {
 	/**
 	 * 输出模板
 	 * @param  string $tpl  模板定位符
-	 * @param  string $suffix 模板文件后缀
 	 */
-	public function display($tpl, $suffix = "") {
-		extract($this->vars, EXTR_OVERWRITE);
-		eval('?>' . $this->parse($this->getTplContent($this->getTplPath($tpl, $suffix))));
+	public function display($tpl = '') {
+		header('Cache-control: ' . $this->option['http_cache_control']); // 页面缓存控制
+		echo $this->fetch($tpl);
+	}
+
+	/**
+	 * 输出模板(js)
+	 * @param string $tpl  模板定位符
+	 * @param boolean  $is_document_write 是否直接js输出
+	 */
+	public function displayJs($tpl = '', $is_document_write = true) {
+		$string = addslashes(str_replace(array("\r", "\n", "\t"), array('', '', ''), $this->fetch($tpl)));
+		return $is_document_write ? 'document.write("' . $string . '");' : $string;
+	}
+
+	/**
+	 * 加载模板
+	 * @param  string $tpl  模板定位符
+	 */
+	public function fetch($tpl = '') {
+		if(!empty($this->tpl_fun)){
+			$func = $this->tpl_fun;
+			$tpl = $func($tpl);
+		}
+		$temp = function ($vars, $content) {
+			ob_start();
+			ob_implicit_flush(0);
+			extract($vars, EXTR_OVERWRITE);
+			eval('?>' . $content);
+			$content = ob_get_clean();
+			return $content;
+		};
+
+		$tpl = trim($tpl, '\\/');
+
+		$tpl_content = '';
+
+		if ($this->checkCache($tpl)) {
+			$tpl_content = file_get_contents($this->getTplCachePath($tpl));
+		} else {
+			$tpl_content = $this->compiler($tpl);
+		}
+		return $temp($this->vars, $tpl_content);
+	}
+
+	/**
+	 * 检测模板缓存是否有效
+	 */
+	private function checkCache($tpl) {
+		$file = $this->getTplPath($tpl);
+
+		// 开启调试
+		if ($this->option['debug']) {
+			return false;
+		}
+
+		// 是否配置缓存目录
+		if (!$this->option['cache_path']) {
+			return false;
+		}
+
+		$cache_tpl_path = $this->getTplCachePath($tpl);
+
+		if (!is_file($cache_tpl_path)) {
+			return false;
+		}
+
+		if (filemtime($file) > filemtime($cache_tpl_path)) {
+			//模板文件如果有更新则缓存需要更新
+			return false;
+		}
+
+		if ($this->option['tpl_cache_time'] != 0 && filemtime($cache_tpl_path) + $this->option['tpl_cache_time'] < time()) {
+			// 缓存是否在有效期
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 编译模板
+	 */
+	private function compiler($tpl) {
+		$tpl_path = $this->getTplPath($tpl);
+		$tpl_content = file_get_contents($tpl_path);
+		// 解析继承模板
+		$tpl_content = $this->parseExtend($tpl_content);
+		// 解析模板语法
+		$tpl_content = $this->parse($tpl_content);
+		// 优化生成的php代码
+		$tpl_content = str_replace('?><?php', '', $tpl_content);
+
+		// 是否配置缓存目录
+		if ($this->option['cache_path']) {
+			$cache_tpl_path = $this->getTplCachePath($tpl);
+			$pathinfo = pathinfo($cache_tpl_path);
+			if (!is_dir($pathinfo['dirname'])) {
+				mkdir($pathinfo['dirname'], 0777, true);
+			}
+			file_put_contents($cache_tpl_path, '<?php if (!defined(\'BAIY_TEMPLATE\')) exit();?>' . $tpl_content);
+		}
+		return $tpl_content;
 	}
 
 	/**
 	 * 获取模板路径
 	 * @param  string $tpl  模板定位符
-	 * @param  string $suffix 模板文件后缀
-	 * 实际使用需要对模板路径做重写
 	 */
-	private function getTplPath($tpl, $suffix = ".html") {
-		if (empty($suffix)) {
-			$suffix = ".html";
-		}
+	private function getTplPath($tpl) {
 		//获取模板位置
-		$tpl_path = $tpl . $suffix;
+		$tpl_path = $this->option['tpl_path'] . $tpl . $this->option['suffix'];
 		if (!is_file($tpl_path)) {
-			throw new Exception("模板不存在");
+			throw new \Exception("模板不存在");
 		}
 		return $tpl_path;
 	}
 
 	/**
-	 * 转义 // 为 /
-	 * @param $var	转义的字符
-	 * @return 转义后的字符
-	 */
-	private function addQuote($var) {
-		return str_replace("\\\"", "\"", preg_replace("/\[([a-zA-Z0-9_\-\.\x7f-\xff]+)\]/s", "['\\1']", $var));
-	}
-
-	/**
-	 * 子模板处理
+	 * 获取模板缓存路径
 	 * @param  string $tpl  模板定位符
-	 * @param  string $suffix 模板文件后缀
 	 */
-	private function LoadSonTpl($tpl, $suffix = '') {
-		if (empty($tpl)) {
-			return '';
-		}
-		return $this->parse($this->getTplContent($this->getTplPath($tpl, $suffix)));
+	private function getTplCachePath($tpl) {
+		return $this->option['cache_path'] . $tpl . $this->option['cache_suffix'];
 	}
 
 	/**
-	 * 获取模板文件内容
-	 * @param  string $tpl_path 模板路径
+	 * 解析继承模板
 	 */
-	private function getTplContent($tpl_path) {
-		return file_get_contents($tpl_path);
+	private function parseExtend($content) {
+		// 读取模板中的继承标签
+		$find = preg_match('/<extend\s(.+?)\s*?\/>/is', $content, $matches);
+		$preg = '/<block\sname=[\'"](.+?)[\'"]\s*?>(.*?)<\/block>/is';
+		if ($find) {
+			// 记录页面中的block标签
+			$block = array();
+			preg_replace_callback($preg,
+				function ($matche) use (&$block) {
+					$block[$matche[1]] = $matche[2];
+					return '';
+				},
+				$content);
+
+			$get_extend_content = function ($string, $ob) {
+				$preg = '/name=[\'"](?<name>.+?)[\'"]/is';
+				preg_match($preg, $string, $matches);
+				return file_get_contents($ob->getTplPath($matches['name']));
+			};
+
+			// 获取模板内容
+			$content = $get_extend_content($matches[1], $this);
+			// 替换对应部分
+			$content = preg_replace_callback($preg,
+				function ($matche) use ($block) {
+					return isset($block[$matche[1]]) ? $block[$matche[1]] : $matche[2];
+				},
+				$content);
+		} else {
+			$content = preg_replace_callback($preg,
+				function ($match) {
+					return $match[2];
+				},
+				$content);
+		}
+		return $content;
 	}
 
 	/**
 	 * 模板解析
 	 */
 	private function parse($str) {
+		$ob = $this;
 		//清除缩进(tab)
 		$str = preg_replace("/([\n\r]+)\t+/s", "\\1", $str);
 
@@ -101,7 +242,9 @@ class Template {
 		$str = preg_replace("/\<\!\-\-\{(.+?)\}\-\-\>/s", "{\\1}", $str);
 
 		//子模板载入 使用方法 {template('public/head')}
-		$str = preg_replace("/\{template\([\'|\"](.+?)[\'|\"]\)\}/ie", "\$this->LoadSonTpl('$1')", $str);
+		$str = preg_replace_callback("/\{template\([\'|\"](.+?)[\'|\"]\)\}/i", function($matche) use($ob){
+			return $ob->parse(file_get_contents($ob->getTplPath($matche[1])));
+		}, $str);
 
 		//文件包含
 		$str = preg_replace("/\{include\s+(.+)\}/", "<?php include \\1; ?>", $str);
@@ -134,7 +277,6 @@ class Template {
 		$str = preg_replace("/\{([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*\(([^{}]*)\))\}/", "<?php echo \\1;?>", $str);
 		$str = preg_replace("/\{\\$([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*\(([^{}]*)\))\}/", "<?php echo \\1;?>", $str);
 		$str = preg_replace("/\{(\\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\}/", "<?php echo \\1;?>", $str);
-		$str = preg_replace("/\{(\\$[a-zA-Z0-9_\[\]\'\"\$\x7f-\xff]+)\}/es", "\$this->addQuote('<?php echo \\1;?>')", $str);
 		$str = preg_replace("/\{([A-Z_\x7f-\xff][A-Z0-9_\x7f-\xff]*)\}/s", "<?php echo \\1;?>", $str);
 		return $str;
 	}
---
+++ test/Template.php
@@ -0,0 +1,14 @@
+<?php
+include 'Init.php';
+
+$option = array(
+		'cache_path' => __DIR__.'/tmp', // 模板缓存路径
+		'tpl_path' => __DIR__.'/template', // 模板路径
+		'debug' => false,
+	);
+$d = 'index/d';
+$template = new \Baiy\Template($option,function($tpl) use($d){
+	return empty($tpl) ? $d : $tpl;
+});
+$template->assign(array("title"=>'自定义标题',"b"=>time()));
+$template->display('index/index');
